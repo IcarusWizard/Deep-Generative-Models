f7e1302b561d61ce12e3979b9583e4a82a7425bc
Icarus <wizardicarus@gmail.com>
2020-03-14 20:31:24 +0800

    update for checkgrad

--- degmo/autoregressive/checkgrad.py
+++ degmo/autoregressive/checkgrad.py
@@ -1,44 +1,57 @@
-from models.autoregressive.pixelrnn import PixelRNN
-from models.autoregressive.pixelcnn import PixelCNN
+from degmo.autoregressive.pixelrnn import PixelRNN
+from degmo.autoregressive.pixelcnn import PixelCNN
 
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 
 def show(img):
     img = img.permute(0, 2, 3, 1).numpy()[0]
     mg = np.abs(img)
+    img = np.max(img, axis=2)
     img = img / np.max(img)
     plt.imshow(img)
     plt.show()
 
-# model = PixelRNN(3, 32, 32, mode='bi', layers=5)
-model = PixelCNN(3, 32, 32, "res", 64, layers=16)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str)
+    args = parser.parse_args()
 
-x = torch.rand(1, 3, 32, 32, requires_grad=True)
+    if args.model == 'bi-rnn':
+        model = PixelRNN(3, 32, 32, mode='bi', layers=3)
+    elif args.model == 'row-rnn':
+        model = PixelRNN(3, 32, 32, mode='row', layers=3)
+    elif args.model == 'res-cnn':
+        model = PixelCNN(3, 32, 32, "res", 64, layers=5)
+    elif args.model == 'gate-cnn':
+        model = PixelCNN(3, 32, 32, "gate", 64, layers=5)
 
-r = model.get_distribution(x)[0]
+    x = torch.rand(1, 3, 32, 32, requires_grad=True)
 
-g = model.get_distribution(x)[1]
+    r = model.get_distribution(x)[0]
 
-b = model.get_distribution(x)[2]
+    g = model.get_distribution(x)[1]
 
-out_grad = torch.zeros_like(r)
-out_grad[:, :, 16, 16] = 1
+    b = model.get_distribution(x)[2]
 
-grad_g = torch.autograd.grad(g, x, out_grad)[0]
+    out_grad = torch.zeros_like(r)
+    out_grad[:, :, 16, 16] = 1
 
-grad_r = torch.autograd.grad(r, x, out_grad)[0]
+    grad_g = torch.autograd.grad(g, x, out_grad)[0]
 
-grad_b = torch.autograd.grad(b, x, out_grad)[0]
+    grad_r = torch.autograd.grad(r, x, out_grad)[0]
 
-show(grad_r)
+    grad_b = torch.autograd.grad(b, x, out_grad)[0]
 
-show(grad_g)
+    show(grad_r)
 
-show(grad_b)
+    show(grad_g)
 
-print('r : {}'.format(grad_r[0, :, 16, 16]))
-print('g : {}'.format(grad_g[0, :, 16, 16]))
-print('b : {}'.format(grad_b[0, :, 16, 16]))
+    show(grad_b)
+
+    print('r : {}'.format(grad_r[0, :, 16, 16]))
+    print('g : {}'.format(grad_g[0, :, 16, 16]))
+    print('b : {}'.format(grad_b[0, :, 16, 16]))
 
